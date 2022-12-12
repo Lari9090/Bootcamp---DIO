e7007a311f29da319cddd61d8c85bcb5093d132e
Lari9090 <larissabio54@gmail.com>
2022-12-12 10:46:16 -0300

    atualizacao

---
+++ JS/desafio01.js
@@ -0,0 +1,9 @@
+//Faça um progama para calcular o preço de uma viagem   
+
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto =  litrosConsumidos*precoCombustivel;
+console.log(valorGasto.toFixed(2));
---
+++ JS/desafio02.js
@@ -0,0 +1,18 @@
+//Faça um progama para calcular o preço de uma viagem   
+
+const precoEtanol = 5.79;
+const precoGasolina = 6.66;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+const tipoCombustivel = 'Etanol';
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+
+if(tipoCombustivel==='Etanol'){
+    
+    const valorGasto =  litrosConsumidos*precoEtanol;
+    console.log(valorGasto.toFixed(2));
+} else{
+    const valorGasto =  litrosConsumidos*precoGasolina;
+    console.log(valorGasto.toFixed(2));
+}
